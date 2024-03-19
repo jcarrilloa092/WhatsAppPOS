278dce2188e5c58d61ae66eaeb9735b277cea43d
Jose Carrillo <jcarrilloa092@ulacit.ed.cr>
2024-03-19 14:17:58 -0600

    Removing old content

--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const whatsappController = require("../controllers/whatsappControllers");
-
-router
-.get("/", whatsappController.VerifyToken)
-.post("/", whatsappController.ReceivedMessage)
-
-module.exports = router;
